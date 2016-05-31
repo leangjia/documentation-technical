c85b3cad94ef14aceca3415f0cc74577e69edd52
jeffery chen fan <jeffery9@gmail.com>
2016-05-31 14:28:17 +0800

    update build tool and README

--- README.md
+++ README.md
@@ -11,6 +11,7 @@
 * 或者在 transifex 平台翻译 https://www.transifex.com/odoo-cn/odoo-documentation-technical/dashboard/
 
 * 从 transifex 下载翻译
+
     tx pull -l zh_CN 
 
 * 生成中文文档
--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 # !/bin/bash
-tx pull -l zh_CN --mode developer
+
+#tx pull -l zh_CN --mode developer
+
 make -e SPHINXOPTS="-D language='zh_CN' -D locale_dirs=locale/"  html
